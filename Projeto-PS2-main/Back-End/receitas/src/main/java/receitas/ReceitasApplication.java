0b5a264b135f281e874137a3fc5a7fad9f3dbdd3
anagierse <144717845+anagierse@users.noreply.github.com>
2025-02-06 16:42:26 -0300

    Update ReceitasApplication.java

--- Projeto-PS2-main/Back-End/receitas/src/main/java/receitas/ReceitasApplication.java
+++ Projeto-PS2-main/Back-End/receitas/src/main/java/receitas/ReceitasApplication.java
@@ -1,7 +1,5 @@
 package receitas;
-//Ana Clara Gierse Raymundo 10428453
-//Luana Domingo Branco 10428459
-//Victor Luiz de Sá Alves 10426310
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
